415762518f16a7a54f978a11c1e9d9cda4a8363f
bruno henrique <bh.bsb2011@hotmail.com>
2023-05-28 23:28:29 -0300

    app structure
    Co-authored-by: JoaoElvas <joaog.elvas@gmail.com>

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,51 @@
+import { useState } from 'react';
 import './App.css';
+import { calculateNoteDispense } from './helpers/calculateNotes';
+import { countNotes } from './helpers/countAllNotes';
 
 function App() {
+  const [input, setInput] = useState(0);
+
+  const notes = calculateNoteDispense(input);
+
+  const parsedNotes = notes.filter(note => note.count !== 0);
+
+  const totalNotes = countNotes(parsedNotes);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <div className="flex">
+        <h1>Smart Notes</h1>
+        <img src="./icon.png" width={100} className="logo" />
+      </div>
+      <div className="card">
+        <h2>Digite quantos reais deseja calcular?</h2>
+        <label className="input">
+          <span className="input__label">R$</span>
+          <input
+            type="number"
+            placeholder="Quantidade"
+            onChange={e => setInput(parseInt(e.target.value))}
+          />
+        </label>
+      </div>
+
+      <h2>Total de notas usadas: {totalNotes}</h2>
+      <div className="notes-list">
+        {parsedNotes.map(({ count, note }) => (
+          <div className="note-card">
+            <h4>{count}</h4>
+            <img
+              src={
+                note === 1
+                  ? './1.jpeg'
+                  : `https://www.bcb.gov.br/content/cedulasemoedas/cedulas_e_moedas/cedulasbc/${note}_front.jpg`
+              }
+              height={100}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
